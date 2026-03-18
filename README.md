3f8153d60962db47a8cd58e6fd43b2540683cd69
Ida <idawestman@outlook.com>
2026-03-18 17:12:49 +0100

    Metoder med tilhørende oppgavebeskrivelser oppdatert

--- README.md
+++ README.md
@@ -5,6 +5,25 @@ Denne oppgaven er en innlevering i DATS2300 - Algoritmer og datastrukturer. Den 
 
 ## Oppgavebeskrivelser
 
+### Oppgave 1
+Dette er så å si helt samme kode som i eksempelet fra kapittel 5.2. Forskjellen er at ved oppretting av et nytt nodeobjekt,
+legges forelderen q inn som parameter i konstruktøren.
+
+I while løkken flytter p og q seg nedover treet ved hjelp av komparatoren som bestemmer om de skal flytte seg mot høyre eller
+venstre for hver node (komparatoren sammenlikner verdien som er oppgitt i metoden med nodens verdi). p blir alltid liggende et
+steg foran q i hver iterasjon. Derfor vet vi at når p havner utenfor treet, er noden ved q forelder, og p har nådd sitt nivå.
+Noden opprettes ved hjelp av konstruktøren i klassen som tar inn verdi og forelder. If setnignen avgjør om noden blir et venstre-
+eller høyrebarn (eller blir rotnode hvis treet er tomt).
+
+### Oppgave 2
+I metoden antall(T verdi) sjekkes det først om treet er tomt eller om verdien vi ser etter er null, og returnerer i såfall 0. Vi kan
+returnere 0 hvis vi ser etter nullverdi, fordi leggInn metoden ikke tillater nullverdier, og vi vet dermed at det ikke finnes i treet.
+
+Videre brukes while løkke og komparatoren til å jobbe seg nedover treet. Hvis T verdi tilsvarer nodeverdien vil komparatoren gi 0, og
+da økes telleren med 1. Pekeren p beveger seg så videre til høyre eller venstre utifra om komparatoren gir negativt tall (T verdi er mindre
+enn nodeverdi så vi går mot venstre), eller positivt tall (T verdi er større eller lik nodeverdien så vi går mot høyre). Letingen stopper når
+p==null og vi er ute av treet.
+
 ### Oppgave 3 - POSTORDEN
 
 #### førstePostorden(Node<T> p)
@@ -13,10 +32,57 @@ ellers til høyre. Når vi kommer til en node uten barn vet vi at det er første
 er bladnoden som er lengst til venstre i treet.
 
 #### nestePostorden(Node<T> p)
-Denne metoden finner neste node i postorden. Merk at p er en node som allerede har blitt besøkt i postorden 
+Denne metoden finner neste node i postorden. Merk at p er en node som allerede har blitt besøkt i postorden
 traverseringen. Her er det flere ulike muligheter:
 * Dersom p er roten, finnes det ingen neste node.
 * Dersom p er et høyrebarn, er neste node p.forelder.
 * Dersom p er venstrebarn uten søsken: neste node er p.forelder.
 * Dersom p er venstrebarn med et høyresøsken: neste node er første node postorden i subrteet med høyresøskenet som rot.
+
+
+### Oppgave 4
+I postOrden(), den ikke rekursive metoden, brukte jeg først førstePostorden(rot) for å finne første node i postorden. I while løkken kalles utførOppgave på den nåværende noden, og deretter
+brukes nestePostorden() til å finne neste node. Siden nestePostorden() returnerer null når noden ikke har foreldre, vil while løkken slutte å iterere etter at oppgaven er utført på rotnoden.
+
+For den rekursive metoden, er tankegangen at for å gå gjennom et binærtre (postorden), må man først gå gjennom hele det venstre subtreet, så det høyre subtreet, så rotnoden selv. Det samme vil
+gjelde for subtrærne, og derfor er det perfekt å bruke rekursjon.
+
+Vi har en base case som returnerer når rot == null. Så har vi et rekursivt kall som kaller metoden med venstrebarnet som rotnode. Rekursjonen vil fortsette hele veien nedover på venstre side,
+helt til det ikke lenger finnes noen venstrebarn, og metoden kalles med rot: null. Dette vil føre til at base casen blir sann, og metoden returnerer. Når en metode er ferdig kjørt, som ved at den returneres,
+fjernes metoden fra call stacken. Dermed er det forrige metodekallet(metodekall x, for forenkling) øverst i stacken. Denne metoden har allerede kjørt postOrdenRekursiv(p.venstre, oppgave), og går dermed videre til neste linje i koden.
+Nå kalles metoden med høyrebarnet som rot. Hvis høyrebarnet eksisterer, vil rekursive kall fortsette til dets subtre har blitt gjennomgått fra venstre til høyre. Hvis det ikke eksisterer, har vi igjen null som rotnode,
+og base casen utløses. Da er vi igjen tilbake i metodekall x. Siden det ikke er flere barn igjen å gå gjennom her, kan oppgaven nå utføres på rotnoden for dette kallet. Kallet fjernes så fra stacken, og vi går tilbake til forrige kall
+der rotnoden er forelderen til noden vi nettopp gjorde oss ferdig med.
+
+### Oppgave 5
+Først brukes komparatoren til å finne fram til noden med gitt verdi. Videre er det 4 ulike tilfeller: p har ingen barn, p har et venstrebarn, p har et høyrebarn eller p har 2 barn.
+
+#### Dersom p ikke har barn:
+Hvis p er roten, settes roten lik null (fordi den hverken har barn eller foreldre er det den eneste noden).
+Hvis p er et venstrebarn, nullstiller vi q sin venstrepeker. Hvis p er et høyrebarn, nullstiller vil q sin høyrepeker.
+
+#### Dersom p har et venstrebarn:
+Hvis p er rot, setter vi barnet lik rot og sletter nodens foreldrepeker.
+Hvis p selv er et venstrebarn, setter vi q.venstre lik p.venstre. p.venstre får q som forelder. Dermed peker q og p.venstre på hverandre, og p forsvinner fra treet.
+Hvis p er et høyrebarn, setter vi i stedet q sin høyrepeker lik p.venstre.
+
+#### Dersom p har et høyrebarn:
+Igjen, hvis p er rot, blir høyrebarnet den nye roten.
+Hvis p er et venstrebarn, setter vi q.venstre lik p.høyre. p.høyre får q som forelder.
+Hvis p er et høyrebarn, setter vi q sin høyrepeker lik p.høyre.
+
+#### Dersom p har 2 barn:
+Her lager vi nye pekere så vi kan finne frem til den nye noden og dens forelder, samtidig som vi bevarer p.
+Vi ønsker å finne den minste verdien i det høyre undertreet, det vil si at vi først tar et skritt til høyre, før vi deretter
+kun beveger oss mot venstre. Derfor blir s = p og r = p.høyre.
+Når vi har beveget oss helt ned til venstre i undertreet, vet vi at vi har kommet fram til riktig node og vi kan gi verdien fra r
+til noden p. Vi vet at r enten har 0 barn, eller et høyrebarn. Dette må vi isåfall flytte opp så det blir barnet til s. Hvis s fortsatt er lik p,
+så vil det være høyrepekeren til s som peker på r.høyre. Hvis vi derimot har beveget oss nedover, vil det være venstrepekeren til s som peker på r.høyre.
+
+#### *
+
+fjernAlle(T verdi) trenger bare å bruke fjern(T verdi) iterativt. Siden fjern() er en boolean metode vil den returnere true så lenge
+den finner en verdi å fjerne, og da vil while løkken fortsette å kjøre.
+
+nullstill() bruker postorden metodene fra tidligere til å gå gjennom treet, og nullstiller alle pekere og verdier.
 
--- src/main/java/no/oslomet/cs/algdat/SøkeBinærTre.java
+++ src/main/java/no/oslomet/cs/algdat/SøkeBinærTre.java
@@ -88,117 +88,195 @@ public class SøkeBinærTre<T>  implements Beholder<T> {
 
     // Oppgave 1
     public boolean leggInn(T verdi) {
-    if (verdi == null){
-        throw new NullPointerException();
-    }
+        if (verdi == null){
+            throw new NullPointerException("Kan ikke legge inn nullverdi");}
 
-    if (antall == 0) rot = new Node<>(verdi, null, null, null);
-    else {
-        Node<T> denne = rot;
-        Node<T> forelder = null;
+        Node<T> p = rot, q = null;
         int cmp = 0;
-        while (denne != null) {
-            forelder = denne;
-            cmp = comp.compare(verdi, denne.verdi);
-            if (cmp < 0) {
-                denne = denne.venstre;
-            }
-            else {
-                denne = denne.høyre;
-            }
+
+        while (p != null)
+        {
+            q = p;
+            cmp = comp.compare(verdi,p.verdi);
+            p = cmp < 0 ? p.venstre : p.høyre;
         }
-        Node <T> nynode = new Node<>(verdi, null, null, forelder);
-        if (cmp < 0) forelder.venstre = nynode;
-        else forelder.høyre = nynode;
-        //Setter høyre/venstre pekeren fra forelder.
-    }
-    antall++;
-    endringer++;
-    return true;
+
+        p = new Node<>(verdi, q);                   // oppretter nodeobjekt med forelder q
+
+        if (q == null) rot = p;
+        else if (cmp < 0) q.venstre = p;
+        else q.høyre = p;
+
+        antall++;
+        return true;
     }
 
 
     // Oppgave 2
-    // Teller antall forekomster av en verdi i treet
     public int antall(T verdi){
-    if (verdi == null) return 0;
-    int teller = 0;
-    Node<T> denne = rot;
 
-    while (denne != null){
-        int cmp = comp.compare(verdi, denne.verdi);
-        if (cmp == 0) {
-            teller ++;
-            denne = denne.høyre;
+        boolean tom = tom();
+        if (tom || verdi==null) return 0;
+
+        int teller = 0;
+        Node<T> p = rot;
+
+        while (p != null) {
+            int cmp = comp.compare(verdi, p.verdi);
+            if (cmp == 0) teller++;
+            if (cmp < 0) p = p.venstre;
+            else p = p.høyre;
         }
-        else if (cmp < 0){
-            denne = denne.venstre;
-        } else denne = denne.høyre;
-    }
-    return teller;
+
+        return teller;
     }
 
     // Oppgave 3
     private Node<T> førstePostorden(Node<T> p) {
-        Node<T> denne = p;
-        while (denne.venstre != null || denne.høyre != null){ //hvis denne har barn
-            if (denne.venstre != null) denne = denne.venstre; //går til venstre hvis mulig;
-            else denne = denne.høyre;
-            }
-        return denne;
+
+        while (true) {  //while(true) kjører helt til den møter på return eller break
+            if (p.venstre != null) p = p.venstre;
+            else if (p.høyre != null) p = p.høyre;
+            else return p;
+        }
     }
 
     private Node<T> nestePostorden(Node<T> p) {
+
         Node<T> neste = null;
-        if (p == rot) return neste;
-        else if (p.forelder.høyre == p) neste = p.forelder;
-        else if (p.forelder.venstre == p){
-            if (p.forelder.høyre == null) neste = p.forelder;
-            else {
-                // neste node er første noden i subtreet der høyresøskenet er rot
-                Node<T> nyrot = p.forelder.høyre;
-                while (nyrot.venstre != null || nyrot.høyre != null){
-                    if (nyrot.venstre != null) nyrot = nyrot.venstre;
-                    else nyrot = nyrot.høyre;
-                }
-                neste = nyrot;
-            }
+
+        if (p.forelder == null) neste = null; //hvis roten er første noden i postorden, finnes det ingen flere noder.
+        else if (p == p.forelder.høyre || p.forelder.høyre == null) neste = p.forelder;
+            //hvis p er høyrebarn ELLER forelderen ikke har høyrebarn, er forelderen neste.
+        else if (p.forelder.høyre != p && p.forelder.høyre != null) { //Forelder har høyrebarn
+            neste = førstePostorden(p.forelder.høyre); //Neste node i postorden er første node i subtreet med høyrebarnet som rot.
         }
+
         return neste;
     }
 
     // Oppgave 4
     public void postOrden(Oppgave<? super T> oppgave) {
-        Node<T> node = førstePostorden(rot);
-        while (node != null) {
-            oppgave.utførOppgave(node.verdi);
-            node = nestePostorden(node);
+        //utførOppgave() skal kjøres på hver node i treet etter postorden.
+
+        if (rot == null) return;
+
+        Node<T> p = førstePostorden(rot);
+        while (p != null){ //siden nestePostorden returnerer null når p er roten, vil while løkken stoppe etter rotnoden.
+            oppgave.utførOppgave(p.verdi);
+            p = nestePostorden(p);
         }
+
     }
 
     public void postOrdenRekursiv(Oppgave<? super T> oppgave) {
         postOrdenRekursiv(rot, oppgave); // Ferdig implementert
     }
+    //Fordi rot er private
 
     private void postOrdenRekursiv(Node<T> p, Oppgave<? super T> oppgave) {
 
-        if (p.venstre == null && p.høyre == null){
-            oppgave.utførOppgave(p.verdi);
-            return;
-        }
-        // base case
+        if (p == null) return; //base case
 
-        if (p.venstre != null){
-            postOrdenRekursiv(p.venstre, oppgave);
-        }
-        if (p.høyre != null) {
-            postOrdenRekursiv(p.høyre, oppgave);
-        }
+        postOrdenRekursiv(p.venstre, oppgave);
+        postOrdenRekursiv(p.høyre, oppgave);
         oppgave.utførOppgave(p.verdi);
     }
 
     // Oppgave 5
-    public boolean fjern(T verdi) { throw new UnsupportedOperationException(); }
-    public int fjernAlle(T verdi) { throw new UnsupportedOperationException(); }
-    public void nullstill() { throw new UnsupportedOperationException(); }
+    public boolean fjern(T verdi) {
+        if (verdi == null) return false;
+        Node<T> p = rot, q = null;
+        while (p != null){
+            int cmp = comp.compare(verdi, p.verdi);
+            if (cmp == 0) {
+                break;
+            }
+            else if (cmp > 0){ //verdi er større enn p.verdi. gå til høyre
+                q = p;
+                p = p.høyre;
+            }
+            else { //verdi er mindre enn p.verdi. gå til venstre
+                q = p;
+                p = p.venstre;
+            }
+            //q er forelder til p.
+        }
+        if (p == null) return false; //verdi ikke funnet.
+        //koden videre vil gjelde for funnet verdi. pekeren p står ved noden, q ved dens forelder.
+        if (p.venstre == null && p.høyre == null) { //p har ingen barn
+            if (p == rot) rot = null;
+            else if (q.venstre == p) q.venstre = null;
+            else q.høyre = null;
+        }
+        else if (p.venstre != null && p.høyre == null){ //p har venstrebarn
+            if (p == rot) {
+                rot = p.venstre;
+                rot.forelder = null;
+            } else if (q.venstre == p) {
+                q.venstre = p.venstre;
+                p.venstre.forelder = q;
+            } else {
+                q.høyre = p.venstre;
+                p.venstre.forelder = q;
+            }
+        } else if (p.venstre == null && p.høyre != null){ //p har høyrebarn
+            if (p == rot) {
+                rot = p.høyre;
+                rot.forelder = null;
+            } else if (q.venstre == p) {
+                q.venstre = p.høyre;
+                p.høyre.forelder = q;
+            } else {
+                q.høyre = p.høyre;
+                p.høyre.forelder = q;
+            }
+        } else { // p har 2 barn
+            Node<T> s = p, r = p.høyre;
+            while (r.venstre != null){
+                s = r; //s er forelder til r
+                r = r.venstre;
+            } //finner minste verdi i høyre undertre
+            p.verdi = r.verdi;
+            if (s == p) { // r er direkte høyrebarn
+                s.høyre = r.høyre;
+                if (r.høyre != null) r.høyre.forelder = s;
+            } else {
+                s.venstre = r.høyre;
+                if (r.høyre != null) r.høyre.forelder = s;
+            }
+        }
+        antall--;
+        return true;
+    }
+
+
+    public int fjernAlle(T verdi) {
+        int teller = 0;
+
+        while (fjern(verdi)) {
+            teller ++;
+        }
+
+        return teller;
+    }
+
+    public void nullstill() {
+        if (rot == null) return;
+
+        Node<T> p = førstePostorden(rot);
+
+        while (p != null) {
+            Node<T> neste = nestePostorden(p);
+
+            p.venstre = null;
+            p.høyre = null;
+            p.forelder = null;
+            p.verdi = null;
+
+            p = neste;
+        }
+
+        rot = null;
+        antall = 0; }
 }
